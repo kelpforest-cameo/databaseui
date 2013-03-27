0da0c234ad3d68b9445a2ee3f4423fb484f1dae3
John Liang <jjliang84@gmail.com>
2013-03-27 16:37:41 -0400

    I manually added all of Luke's changes to the most recent version of the code, closing Luke's pull request now.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 FoodWebBuilder::Application.routes.draw do
 
   authenticated :user do
-  	root :to => 'dashboard#index'
+  	root :to => 'dashboard#index' 
   end
   root :to =>"home#index"
   devise_for :users
@@ -9,6 +9,7 @@ FoodWebBuilder::Application.routes.draw do
   match 'about' => 'home#about'
   match 'help' => 'home#help'
   match 'visualization' => 'home#visuals'
+  match 'dataentry' => 'dashboard#dataentry'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
