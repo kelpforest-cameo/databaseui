b80aea46631e13e608be2542d52c16d5b12abd6f
John Liang <jjliang84@gmail.com>
2013-03-29 14:20:21 -0400

    added some roles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130329153012) do
+ActiveRecord::Schema.define(:version => 20130329162335) do
 
   create_table "author_cites", :force => true do |t|
     t.integer  "author_id"
@@ -496,6 +496,7 @@ ActiveRecord::Schema.define(:version => 20130329153012) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "role"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
