b0f80e94d622e24a2e4de208af3d66747a5fdca9
John Liang <jjliang84@gmail.com>
2013-04-01 13:03:54 -0400

    fixed typo in visualizations tab

--- app/controllers/visualization_controller.rb
+++ app/controllers/visualization_controller.rb
@@ -2,7 +2,7 @@ class VisualizationController < ApplicationController
 def index
  @chart = LazyHighCharts::HighChart.new('graph') do |f|
     f.title({ :text=>"Foodweb Ladder Rankings"})
-    f.options[:xAxis][:categories] = ['Citatiosn']
+    f.options[:xAxis][:categories] = ['Citations']
 User.find_each do |user|
 f.series(:type=> 'column',:name=> user.email,:data=> [Citation.where(:user_id => user.id ).count ])
 end  
