b98e6adb319b52e5f51776476f5f3b972f7fa6ca
John Liang <jjliang84@gmail.com>
2013-04-03 10:50:20 -0300

    fixing dashboard controller
    Latest pull request causes errors commented out the code for now

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,15 +1,15 @@
 class DashboardController < ApplicationController
  
  def index
-  @cities = City.all
-  @json = @cities.to_gmaps4rails do |city, marker|
-  marker.infowindow render_to_string(:partial => "/cities/infowindow", :locals => { :city => city})
-    marker.title "#{city.name}"
-    marker.json({ :population => city.population})
-    marker.picture({:picture => "http://mapicons.nicolasmollet.com/wp-content/uploads/mapicons/shape-default/color-3eab71/shapecolor-color/shadow-1/border-dark/symbolstyle-contrast/symbolshadowstyle-dark/gradient-iphone/mushroom.png",
-                    :width => 32,
-                    :height => 32})
-  end
+  #@cities = City.all
+  #@json = @cities.to_gmaps4rails do |city, marker|
+  #marker.infowindow render_to_string(:partial => "/cities/infowindow", :locals => { :city => city})
+    #marker.title "#{city.name}"
+    #marker.json({ :population => city.population})
+    #marker.picture({:picture => "http://mapicons.nicolasmollet.com/wp-content/uploads/mapicons/shape-default/color-3eab71/shapecolor-color/shadow-1/border-dark/symbolstyle-contrast/symbolshadowstyle-dark/gradient-iphone/mushroom.png",
+      #              :width => 32,
+       #             :height => 32})
+  #end
 end
   
   def dataentry
@@ -18,7 +18,7 @@ end
   @citation = Citation.new
   @citationlist = Citation.all
   @node = Node.new
-  @non_iti = Non_Iti.new
+  @non_iti = NonIti.new
   @nodelist = Node.all
   end
 
