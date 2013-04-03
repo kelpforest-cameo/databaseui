f56340696daaea37e8e1218dc817c2eb42a04e45
Daisuke Tanaka <diceydawg@gmail.com>
2013-04-03 00:35:58 -0400

    Added googlemap in dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,16 @@
 class DashboardController < ApplicationController
  
-  def index
+ def index
+  @cities = City.all
+  @json = @cities.to_gmaps4rails do |city, marker|
+  marker.infowindow render_to_string(:partial => "/cities/infowindow", :locals => { :city => city})
+    marker.title "#{city.name}"
+    marker.json({ :population => city.population})
+    marker.picture({:picture => "http://mapicons.nicolasmollet.com/wp-content/uploads/mapicons/shape-default/color-3eab71/shapecolor-color/shadow-1/border-dark/symbolstyle-contrast/symbolshadowstyle-dark/gradient-iphone/mushroom.png",
+                    :width => 32,
+                    :height => 32})
   end
+end
   
   def dataentry
   @author = Author.new
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
  users = User.create(:email => 'foodwebbuilder@gmail.com', :password => '12345678', :password_confirmation => '12345678', :role => 'admin', :approved => 1)
+
+
+
+City.create(name: "New york" ,state: "NY", population: 8175133)
+City.create(name: "Los Angeles",state: "CA", population: 3792621 )
+City.create(name: "Chicago",state: "IL", population: 2695598)
+City.create(name: "Houston",state: "TX", population: 2099451)
+City.create(name: "Philadelphia",state: "PA" , population: 1526006)
+City.create(name: "Phoenix",state: "AZ", population: 1445632)
+City.create(name: "San Antonio",state: "TX", population: 1327407)
+City.create(name: "San Diego",state: "CA", population: 1307402 )
+City.create(name: "Dallas",state: "TX", population: 1197816)
+City.create(name: "San Jose",state: "CA", population: 945942)
