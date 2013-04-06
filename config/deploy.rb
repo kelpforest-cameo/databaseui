38860da4ecccbf427875a01a3326b228add7b542
John Liang <jjliang84@gmail.com>
2013-04-06 17:23:17 -0400

    testing changes to see if cap works

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,8 @@
 # -*- coding: iso-8859-1 -*-
 require 'capistrano/ext/multistage'
-require "bundler/capistrano"
+#require "bundler/capistrano"
 require 'capistrano-unicorn'
 set :application, "FoodWebBuilder"
-
-
 
 default_run_options[:pty] = true  # Must be set for the password prompt from #git to work    
 set :repository, "https://github.com/jjliang/databaseui.git"  # Your clone URL
