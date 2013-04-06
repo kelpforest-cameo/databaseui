5ad42e96a332525ba0b95726ca6cbddaa4971bca
John Liang <jjliang84@gmail.com>
2013-04-06 18:02:10 -0400

    testing changes to see if cap works

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,7 +9,7 @@
 # documentation.
 
 # Setting the application path
-app_path = "/var/rails/fwb"
+app_path = "/var/rails/fwb/current"
 
 # Use at least one worker per core if you're on a dedicated server,
 # more will usually help for _short_ waits on databases/caches.
@@ -27,24 +27,24 @@ rails_env = ENV['RAILS_ENV'] || 'production'
 
 # Help ensure your application will always spawn in the symlinked
 # "current" directory that Capistrano sets up.
-working_directory "/var/rails/fwb/" # available in 0.94.0+
+working_directory "/var/rails/fwb/current" # available in 0.94.0+
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
-listen "/var/rails/fwb/tmp/unicorn.sock", :backlog => 64
+listen "/var/rails/fwb/current/tmp/unicorn.sock", :backlog => 64
 listen 8000, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 180
 
 # feel free to point this anywhere accessible on the filesystem
-pid "/var/rails/fwb/shared/pids/unicorn.pid"
+pid "/var/rails/fwb/current/shared/pids/unicorn.pid"
 
 # By default, the Unicorn logger will write to stderr.
 # Additionally, ome applications/frameworks log to stderr or stdout,
 # so prevent them from going to /dev/null when daemonized here:
-stderr_path "/var/rails/fwb/shared/log/unicorn.stderr.log"
-stdout_path "/var/rails/fwb/shared/log/unicorn.stdout.log"
+stderr_path "/var/rails/fwb/current/shared/log/unicorn.stderr.log"
+stdout_path "/var/rails/fwb/current/shared/log/unicorn.stdout.log"
 
 # combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
 # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
