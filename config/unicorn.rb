fcc23a3fcd0c2758a85a0e40a7a5bf75acf6353d
John Liang <jjliang84@gmail.com>
2013-04-08 22:45:02 -0400

    added some changes

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -38,7 +38,7 @@ listen 8000, :tcp_nopush => true
 timeout 180
 
 # feel free to point this anywhere accessible on the filesystem
-pid "/var/rails/fwb/current/shared/pids/unicorn.pid"
+pid "/var/rails/fwb/shared/pids/unicorn.pid"
 
 # By default, the Unicorn logger will write to stderr.
 # Additionally, ome applications/frameworks log to stderr or stdout,
