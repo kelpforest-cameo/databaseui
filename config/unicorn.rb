1b8dbc5208f61542277455e50a2ea62624e7905b
John Liang <jjliang84@gmail.com>
2013-04-08 22:46:22 -0400

    added some changes

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -43,8 +43,8 @@ pid "/var/rails/fwb/shared/pids/unicorn.pid"
 # By default, the Unicorn logger will write to stderr.
 # Additionally, ome applications/frameworks log to stderr or stdout,
 # so prevent them from going to /dev/null when daemonized here:
-stderr_path "/var/rails/fwb/current/shared/log/unicorn.stderr.log"
-stdout_path "/var/rails/fwb/current/shared/log/unicorn.stdout.log"
+stderr_path "/var/rails/fwb/shared/log/unicorn.stderr.log"
+stdout_path "/var/rails/fwb/shared/log/unicorn.stdout.log"
 
 # combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
 # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
