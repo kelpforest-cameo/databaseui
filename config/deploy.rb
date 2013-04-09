7471a5b9a4b979ff6c23e62fe5852f95e8818748
John Liang <jjliang84@gmail.com>
2013-04-08 21:57:48 -0400

    added some changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :scm_password, Proc.new { Capistrano::CLI.password_prompt "SCM Password: "}
 
 role :web, "fwb.cs.umb.edu"
 role :app, "fwb.cs.umb.edu"
-role :db, "vm77.cs.umb.edu"
+role :db, "localhost"
 
 set :rails_env, :production
 set :unicorn_binary, "/var/rails/fwb/bin/bootup_unicorn"
@@ -33,53 +33,7 @@ namespace :deploy do
 	task :start, :roles => :app, :except => { :no_release => true} do
 		run "cd#{current_path}&&#{try_sudo}#{unicorn_binary}-c#{unicorn_config}-E#{rails_env}-D"
 	end
+	
 end
 
 
-
-
-
-
-#Define stage and productipn environments
-#set :stages, ["staging", "production"]
-#set :default_stage, "staging"
-
-#after "deploy:restart", "deploy:cleanup" #clean up old releases on each deploy uncomment this
-
-# =============================================================================
-# TASKS 
-# =============================================================================
-
-#  Multiple Stages Without Multistage Extension
-#  https://github.com/capistrano/capistrano/wiki/2.x-Multiple-Stages-Without-Multistage-Extension
-#
-#  cap production deploy - to deploy to prod. / deploy to stage => cap deploy
-#  cap production deploy:restart
-#task :production do
-#  role :web, "vm77.cs.umb.edu"   # Your HTTP server, Apache/etc (where your web server software runs)
-#  role :app, "vm77.cs.umb.edu"   # This may be the same as your `Web` server
-#  role :db,  "127.0.0.0.1", :primary => true 	# This is where Rails migrations will run 
-#  set :deploy_to, "/var/rails/fwb" #specify where on the server our application resides 
-#  set :deploy_via, :remote_cache # only copy the most recent, not the entire repository
-#  set :branch, 'staging' #branch to checkout during deployment
-#end
-
-#task :staging do
-#  role :web, "vm77.cs.umb.edu"   # Your HTTP server, Apache/etc (where your web server software runs)
-#  role :app, "vm77.cs.umb.edu"   # This may be the same as your `Web` server
-#  role :db,  "vm77.cs.umb.edu", :primary => true 	# This is where Rails migrations will run 
-#  set :deploy_to, "/var/rails/fwb"
-#  set :deploy_via, :remote_cache # only copy the most recent, not the entire repository
-#  set :branch, 'staging' #branch to checkout during deployment
-
-#end
-
-# =============================================================================
-# SSH OPTIONS
-# =============================================================================
-# ssh_options[:keys] = %w(/path/to/my/key /path/to/another/key)
-# ssh_options[:port] = 25
-#If you're using your own private keys for git, you want to tell Capistrano 
-#to use agent forwarding with this command. Agent forwarding can make key management
-#much simpler as it uses your local keys instead of keys installed on the server
-# set :ssh_options, { :forward_agent => true, :paranoid => false }
