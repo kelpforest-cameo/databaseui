8deb3c18fbd2f2240d3d305dadae512060ef9648
John Liang <jjliang84@gmail.com>
2013-04-09 15:37:08 -0400

    changing to backticks

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,13 +37,13 @@ namespace :deploy do
 	end 
 	
 	task :stop, :roles => :app, :except => { :no_release => true} do
-		run "#{try_sudo} kill 'cat #{unicorn_pid}'"
+		run "#{try_sudo} kill `cat #{unicorn_pid}`"
 	end
 	task :graceful_stop, :roles => :app, :except => { :no_release => true} do
-		run "#{try_sudo} kill -s QUIT 'cat #{unicorn_pid}'"
+		run "#{try_sudo} kill -s QUIT `cat #{unicorn_pid}`"
 	end
 	task :reload, :roles => :app, :except => { :no_release => true} do
-		run "#{try_sudo} kill -s USR2 'cat #{unicorn_pid}'"
+		run "#{try_sudo} kill -s USR2 `cat #{unicorn_pid}`"
 	end
 	task :restart, :roles => :app, :except => { :no_release => true} do
 	 stop
