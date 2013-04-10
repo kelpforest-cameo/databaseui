1f8000c68c0a6b4bdee9658f59cfd107fe4c9508
John Liang <jjliang84@gmail.com>
2013-04-10 13:50:10 -0400

    trying to run db:setup from capistrano

--- config/deploy/capistrano_database.rb
+++ config/deploy/capistrano_database.rb
@@ -139,7 +139,6 @@ Capistrano::Configuration.instance.load do
         run "mkdir -p #{shared_path}/db" 
         run "mkdir -p #{shared_path}/config" 
         put config.result(binding), "#{shared_path}/config/database.yml"
-        run "rake db:setup"
       end
 
       desc <<-DESC
