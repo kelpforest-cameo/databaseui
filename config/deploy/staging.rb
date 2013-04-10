b3f4c1f2f58c1cbad7eef003d619ef1d8ebed192
John Liang <jjliang84@gmail.com>
2013-04-10 14:55:19 -0400

    trying to create task for db:setup

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,7 @@ set :rails_env, :staging
     after "deploy:update_code", :dbsetup
     after "deploy:dbsetup", :precompile_assets
     desc "precompile the assets"
-    task :dbsetup, :roles => :app, :only => {:no_release => true} do
+    task :dbsetup, :roles => :app, :except => {:no_release => false} do
     	run "cd #{release_path} && RAILS_ENV = #{rails_env} bundle exec rake db:setup"
     end
     task :precompile_assets, :roles => :app do
