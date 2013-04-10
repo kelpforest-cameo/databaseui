3256171c4ef86c450e819e50456abecaa7234cd4
John Liang <jjliang84@gmail.com>
2013-04-10 13:18:06 -0400

    removing extra spacing lines

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,10 +1,5 @@
 FoodWebBuilder::Application.configure do
 
-
-
-
-
-
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
