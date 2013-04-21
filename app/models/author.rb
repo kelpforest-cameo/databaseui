54e4eb1e48c8078da529c87b6628b27c8dbbfb86
John Liang <jjliang84@gmail.com>
2013-04-21 12:25:02 -0400

    moved the nav bar

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,5 +4,8 @@ class Author < ActiveRecord::Base
   belongs_to :project
   has_many :author_cites
   has_many :citations, :through => :author_cites, :dependent => :destroy
+  
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
 end
 
