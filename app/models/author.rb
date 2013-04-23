a4ce17e559c3690bc531fc03bdac8ef4e1f024db
Kevin W. O'Brien <kevb0001@gmail.com>
2013-04-23 11:33:44 -0400

    'work_in_progress'

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,5 +4,9 @@ class Author < ActiveRecord::Base
   belongs_to :project
   has_many :author_cites
   has_many :citations, :through => :author_cites, :dependent => :destroy
+
+  def full_name
+ 	 self.first_name + " " + self.last_name
+  end
 end
 
