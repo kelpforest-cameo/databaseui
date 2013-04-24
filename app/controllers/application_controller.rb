d31c163b0e8ed2968e2a6f9fd6e47720825166e4
John Liang <jjliang84@gmail.com>
2013-04-24 13:24:53 -0400

    added listing nodes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,8 +19,15 @@ class ApplicationController < ActionController::Base
 		def owner_user(id)
 			@owner_user ||= User.find(id)
 		end
+  
+	# Helper method to disply functional groups
+	helper_method :functional_name
+		def functional_name(id)
+			@functional_name ||= FunctionalGroup.find(id)
+		end
 		
     rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
+  
 end
