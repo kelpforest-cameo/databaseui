bfa26374c0e4cb92f80ea0bd9ab87f6ee2b974a8
John Liang <jjliang84@gmail.com>
2013-04-26 14:08:57 -0400

    adding authors fields to citations

--- app/models/author.rb
+++ app/models/author.rb
@@ -8,5 +8,9 @@ class Author < ActiveRecord::Base
   validates :first_name, :presence => true
   validates :last_name, :presence => true 
   
+  def listing_name
+  	"#{last_name} #{first_name}"
+  end
+  
 end
 
