9d195f2cafa76028716a23363fba823d2b27fd8a
John Liang <jjliang84@gmail.com>
2013-04-28 15:42:25 -0400

    trying to precompile feedback.css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,7 @@ FoodWebBuilder::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
+	config.assets.precompile += ['feedback.css']
   # Generate digests for assets URLs
   config.assets.digest = true
 
