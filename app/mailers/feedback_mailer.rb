51e02265d1e366bc804ded5885ebc98ad2b1f771
John Liang <jjliang84@gmail.com>
2013-04-28 22:20:27 -0400

    changing mail to

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -3,6 +3,6 @@ class FeedbackMailer < ActionMailer::Base
 
   def feedback(feedback)
     @feedback = feedback
-    mail(:to => 'jjliang84@gmail.com', :cc => 'diceydawg@gmail.com', :subject => 'Feedback for FoodWebBuilder')
+    mail(:to => 'jjliang84@gmail.com', :subject => 'Feedback for FoodWebBuilder')
   end
 end
