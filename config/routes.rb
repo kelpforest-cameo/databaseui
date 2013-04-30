d214fd898d919a5957c45f99fd0c7380d4b1fac4
John Liang <jjliang84@gmail.com>
2013-04-30 12:01:20 -0400

    fixed page numbers and added actions

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ FoodWebBuilder::Application.routes.draw do
 
 
 	#For Node Search
-	
 	match 'search' => 'dashboard#search', :as => "search"
 	match 'search_by_tsn' => 'nodes#search_by_tsn'
 	# for interactions
@@ -94,6 +93,6 @@ FoodWebBuilder::Application.routes.draw do
   match 'dataentry' => 'dashboard#dataentry'
 	match 'edit_user_path' => 'users_registrations#edit'
 	match 'home' => 'home#index'
-	match 'dashboard' => 'dashboard#index'
+
 	
 end
